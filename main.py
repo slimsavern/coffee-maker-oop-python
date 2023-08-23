50acb2455e3d3c1c6953ff5331fbfefd548b2e5b
Elias Bansi <slimsavern@gmail.com>
2023-08-23 11:52:19 +0000

    Shortend, cleaner

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ turn_off_machine = False
 
 while not turn_off_machine:
     choice = input(f"What would you like? {menu.get_items()}: ").lower()
-
     if choice == 'off':
         turn_off_machine = True
     elif choice == 'report':
@@ -18,6 +17,5 @@ while not turn_off_machine:
         money_machine.report()
     else:
         chosen_drink = menu.find_drink(choice)
-        if coffee_make.is_resource_sufficient(chosen_drink):
-            if money_machine.make_payment(chosen_drink.cost):
-                coffee_make.make_coffee(chosen_drink)
+        if coffee_make.is_resource_sufficient(chosen_drink) and money_machine.make_payment(chosen_drink.cost):
+            coffee_make.make_coffee(chosen_drink)
